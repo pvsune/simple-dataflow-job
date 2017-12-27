f6dd7b57493a905efacd68fe9953f09b6bbe08b6
Philamer Sune <phil@pez.ai>
2017-12-27 14:50:16 +0800

    Fix condition to fail if headers is not True.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 @get('/pipeline')
 def index():
-    if request.get_header('x-pipeline-cron', None) == 'True':
+    if request.get_header('x-pipeline-cron', None) != 'True':
         return 'You are not allowed to do this!'
 
     input_ = 'gs://test-pez-ai/mydataflow_2017-12-19T08:55:00.000Z-2017-12-19T09:00:00.000Z-pane-0-last-00-of-01'
